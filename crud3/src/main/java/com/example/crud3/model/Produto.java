1da7b02a95dab7aed6431bd8b2191ba315f0bec9
alunosenai <alunosenai@senaiba.com.br>
2025-05-17 11:35:36 -0300

    Upload 1.0.0

---
+++ crud3/src/main/java/com/example/crud3/model/Produto.java
@@ -0,0 +1,60 @@
+package com.example.crud3.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Double preco;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    public Produto() {}
+
+    public Produto(String nome, Double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
